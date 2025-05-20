162a43ffb6defb8764c1c25b143d28076c7cd6f5
Nathen <nagapri16@gmail.com>
2025-05-20 15:47:31 +0700

    Fixed mobile navigation pop up and modal pop up

--- shared.js
+++ shared.js
@@ -1,3 +1,38 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var selectPlanButton = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
-backdrop.style.display = 'block';
+
+for (var i = 0; i < selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener('click', function() {
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; // This will override the complete class list.
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+    })
+}
+
+backdrop.addEventListener('click', function() {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+})
